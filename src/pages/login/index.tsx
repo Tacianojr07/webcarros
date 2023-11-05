e1ac3a921f2bcf0be20a2f83a5ed1ef9fd5e4c06
Tacianojr07 <t.juniior13@gmail.com>
2023-11-05 17:43:08 -0300

    structure, styled and react-hook-form

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,42 @@
+import { Container } from "../../components/container";
+import logoImg from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
+import { Input } from "../../components/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const schema = z.object({
+  email: z
+    .string()
+    .email("Insira um email válido")
+    .min(1, { message: "insira um email valido" }),
+  password: z.string().min(1, { message: "a senha não pode ser nula" }),
+});
+
+type FormData = z.infer<typeof schema>;
+
 export function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  });
+
   return (
-    <div>
-      <h1>page home</h1>
-    </div>
+    <Container>
+      <div className="min-h-screen w-full flex flex-col justify-center items-center">
+        <Link className="w-full max-w-sm mb-6" to="/">
+          <img className="w-full" src={logoImg} alt="logo" />
+        </Link>
+
+        <form className="w-full max-w-sm rounded-lg">
+          <Input />
+        </form>
+      </div>
+    </Container>
   );
 }
