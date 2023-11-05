e889df295cc9c70266f65dc78fa7108c23e0a50f
Tacianojr07 <t.juniior13@gmail.com>
2023-11-05 11:11:44 -0300

    :lipstick: feat: styled header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,34 @@
+import { Link } from "react-router-dom";
+import { FiUser, FiLogIn } from "react-icons/fi";
+import logosvg from "../../assets/logo.svg";
+
 export function Header() {
+  const signed = false;
+  const loadingAuth = false;
+
   return (
-    <div>
-      <h1>page header</h1>
+    <div className="w-full h-16 flex justify-center items-center mb-4 bg-white drop-shadow-sm">
+      <header className="w-full max-w-7xl flex justify-between items-centermx-auto px-4">
+        <Link to="/">
+          <img src={logosvg} alt="logo" />
+        </Link>
+
+        {!loadingAuth && signed && (
+          <Link to="/dashboard">
+            <div className="p-1 border-2 rounded-full border-black">
+              <FiUser size={26} color="#000" />
+            </div>
+          </Link>
+        )}
+
+        {!loadingAuth && !signed && (
+          <Link to="/login">
+            <div className="p-1 border-2 rounded-full border-black">
+              <FiLogIn size={26} color="#000" />
+            </div>
+          </Link>
+        )}
+      </header>
     </div>
   );
 }
