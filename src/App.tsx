4ca0294d86f4d05088147513abf0bf10b172e57b
Tacianojr07 <t.juniior13@gmail.com>
2023-11-05 09:49:12 -0300

    :sparkles: feat: creating routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
-function App() {
-  return (
-    <div>
-      <h1>olá mundo</h1>
-    </div>
-  );
-}
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages/home";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+import { Dashboard } from "./pages/dashboard";
+import { New } from "./pages/dashboard/new";
+import { Car } from "./pages/car";
+import { Layout } from "./components/layout";
 
-export default App;
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/dashboard/new",
+        element: <New />,
+      },
+      {
+        path: "/car",
+        element: <Car />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
+export { router };
