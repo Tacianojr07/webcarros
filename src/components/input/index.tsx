7dae2b3577f445b566c037908110fd328ab0f53c
Tacianojr07 <t.juniior13@gmail.com>
2023-11-05 17:42:14 -0300

    add interface

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,7 +1,18 @@
-export function Input() {
+interface InputProps {
+  name: string;
+  placeholder: string;
+  type: string;
+}
+
+export function Input({ name, placeholder, type }: InputProps) {
   return (
     <div>
-      <input placeholder="digite seu email" />
+      <input
+        className="w-full h-11 border-2 px-2 rounded-lg"
+        type={type}
+        name={name}
+        placeholder={placeholder}
+      />
     </div>
   );
 }
