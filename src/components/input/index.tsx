c393dd43bc2720cec78700cb257d67c0cd6019a5
Tacianojr07 <t.juniior13@gmail.com>
2023-11-07 09:29:30 -0300

    add types and modified component

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,18 +1,33 @@
+import { UseFormRegister, RegisterOptions } from "react-hook-form";
+
 interface InputProps {
   name: string;
   placeholder: string;
   type: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  register: UseFormRegister<any>;
+  error?: string;
+  rules?: RegisterOptions;
 }
 
-export function Input({ name, placeholder, type }: InputProps) {
+export function Input({
+  name,
+  placeholder,
+  type,
+  register,
+  rules,
+  error,
+}: InputProps) {
   return (
     <div>
       <input
         className="w-full h-11 border-2 px-2 rounded-lg"
         type={type}
-        name={name}
         placeholder={placeholder}
+        {...register(name, rules)}
+        id={name}
       />
+      {error && <p>{error}</p>}
     </div>
   );
 }
