b1af45facd14d8b1f74b7229fd2b24c4b924e84a
Tacianojr07 <t.juniior13@gmail.com>
2023-11-09 10:51:29 -0300

    upload of image

--- src/pages/dashboard/new/index.tsx
+++ src/pages/dashboard/new/index.tsx
@@ -1,7 +1,30 @@
+import { Container } from "../../../components/container";
+import { DashboardHeaderl } from "../../../components/painelheader";
+import { FiUpload } from "react-icons/fi";
+
 export function New() {
   return (
-    <div>
-      <h1>page home</h1>
-    </div>
+    <Container>
+      <DashboardHeaderl />
+
+      <div className="w-full bg-white rounded-lg p-3 flex flex-col  sm:flex-row items-center gap-3">
+        <button className="w-48 border-2 flex items-center justify-center rounded-lg cursor-pointer border-gray-600 h-32 md:48">
+          <div className="absolute cursor-pointer">
+            <FiUpload size={28} color="#000" />
+          </div>
+          <div>
+            <input
+              type="file"
+              accept="image/*"
+              className="opacity-0 cursor-pointer"
+            />
+          </div>
+        </button>
+      </div>
+
+      <div className="w-full bg-white flex flex-col p-3 rounded-lg sm:flex-row items-center gap-3 mt-4">
+        <h1>tesetttts</h1>
+      </div>
+    </Container>
   );
 }
