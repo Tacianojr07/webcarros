579ac7cd30c49521280afd26feedadb37feca262
Tacianojr07 <t.juniior13@gmail.com>
2023-11-09 10:31:31 -0300

    creating component painelheader

--- src/components/painelheader/index.tsx
+++ src/components/painelheader/index.tsx
@@ -1,18 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { auth } from "../../services/firebaseConnection";
 
 export function DashboardHeaderl() {
+  const navigate = useNavigate();
   async function handleLogout() {
     await signOut(auth);
+    navigate("/");
   }
 
   return (
-    <div>
+    <div className="w-full bg-red-500 h-10 flex items-center gap-4 px-4 text-white font-medium rounded-lg mb-4">
       <Link to="/dashboard">Dahsboard</Link>
       <Link to="/dashboard/new">Cadastrar carro</Link>
 
-      <button onClick={handleLogout}>Sair da conta</button>
+      <button className="ml-auto" onClick={handleLogout}>
+        Sair da conta
+      </button>
     </div>
   );
 }
