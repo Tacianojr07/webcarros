4b1e628aaf3e414d80c63314e1af56a6368dc468
Tacianojr07 <t.juniior13@gmail.com>
2023-11-12 10:12:21 -0300

    searching for item in db and rendering on screen

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,56 @@
+import { useState, useEffect } from "react";
 import { Container } from "../../components/container";
+import { collection, query, getDocs, orderBy } from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
+
+interface CarProps {
+  id: string;
+  name: string;
+  year: string;
+  km: string;
+  uid: string;
+  city: string;
+  price: string | number;
+  carImages: CarImageProps[];
+}
+
+interface CarImageProps {
+  name: string;
+  uid: string;
+  url: string;
+}
 
 export function Home() {
+  const [car, setCar] = useState<CarProps[]>([]);
+
+  useEffect(() => {
+    function loadCards() {
+      const carsRef = collection(db, "cars");
+      const queryRef = query(carsRef, orderBy("date", "desc"));
+
+      getDocs(queryRef).then((snapshot) => {
+        const carList = [] as CarProps[];
+
+        snapshot.forEach((doc) => {
+          carList.push({
+            id: doc.id,
+            name: doc.data().name,
+            year: doc.data().year,
+            km: doc.data().km,
+            price: doc.data().price,
+            city: doc.data().city,
+            carImages: doc.data().carImages,
+            uid: doc.data().uid,
+          });
+        });
+
+        setCar(carList);
+      });
+    }
+
+    loadCards();
+  }, []);
+
   return (
     <Container>
       <section className="bg-white w-full max-w-3xl p-3 rounded-lg mx-auto flex justify-center items-center  gap-3">
@@ -18,94 +68,30 @@ export function Home() {
       </h1>
 
       <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <section className="w-full bg-white rounded-lg">
-          <img
-            className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 cursor-pointer transition-all "
-            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202310/20231026/honda-civic-2.0-16v-flexone-exl-4p-cvt-wmimagem15252259135.jpg?s=fill&w=552&h=414&q=60"
-            alt="carro"
-          />
+        {car.map((item) => (
+          <section key={item.id} className="w-full bg-white rounded-lg">
+            <img
+              className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 cursor-pointer transition-all "
+              src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202310/20231026/honda-civic-2.0-16v-flexone-exl-4p-cvt-wmimagem15252259135.jpg?s=fill&w=552&h=414&q=60"
+              alt="carro"
+            />
 
-          <p className="font-bold mt-1 mb-2 px-2">CIVIC</p>
+            <p className="font-bold mt-1 mb-2 px-2">CIVIC</p>
 
-          <div className="flex flex-col px-2">
-            <span className="text-zinc-700 mb-7 ">Ano 2016 | 23.000 KM</span>
-            <strong className="text-black text-xl font-medium">
-              R$ 190.000
-            </strong>
+            <div className="flex flex-col px-2">
+              <span className="text-zinc-700 mb-7 ">Ano 2016 | 23.000 KM</span>
+              <strong className="text-black text-xl font-medium">
+                R$ 190.000
+              </strong>
 
-            <div className="w-full h-px bg-slate-300 my-2"></div>
+              <div className="w-full h-px bg-slate-300 my-2"></div>
 
-            <div className="px-2 pb-2">
-              <span className="text-black">Toritama-PE</span>
+              <div className="px-2 pb-2">
+                <span className="text-black">Toritama-PE</span>
+              </div>
             </div>
-          </div>
-        </section>
-        <section className="w-full bg-white rounded-lg">
-          <img
-            className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 cursor-pointer transition-all "
-            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202310/20231026/honda-civic-2.0-16v-flexone-exl-4p-cvt-wmimagem15252259135.jpg?s=fill&w=552&h=414&q=60"
-            alt="carro"
-          />
-
-          <p className="font-bold mt-1 mb-2 px-2">CIVIC</p>
-
-          <div className="flex flex-col px-2">
-            <span className="text-zinc-700 mb-7 ">Ano 2016 | 23.000 KM</span>
-            <strong className="text-black text-xl font-medium">
-              R$ 190.000
-            </strong>
-
-            <div className="w-full h-px bg-slate-300 my-2"></div>
-
-            <div className="px-2 pb-2">
-              <span className="text-black">Toritama-PE</span>
-            </div>
-          </div>
-        </section>
-        <section className="w-full bg-white rounded-lg">
-          <img
-            className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 cursor-pointer transition-all "
-            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202310/20231026/honda-civic-2.0-16v-flexone-exl-4p-cvt-wmimagem15252259135.jpg?s=fill&w=552&h=414&q=60"
-            alt="carro"
-          />
-
-          <p className="font-bold mt-1 mb-2 px-2">CIVIC</p>
-
-          <div className="flex flex-col px-2">
-            <span className="text-zinc-700 mb-7 ">Ano 2016 | 23.000 KM</span>
-            <strong className="text-black text-xl font-medium">
-              R$ 190.000
-            </strong>
-
-            <div className="w-full h-px bg-slate-300 my-2"></div>
-
-            <div className="px-2 pb-2">
-              <span className="text-black">Toritama-PE</span>
-            </div>
-          </div>
-        </section>
-        <section className="w-full bg-white rounded-lg">
-          <img
-            className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 cursor-pointer transition-all "
-            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202310/20231026/honda-civic-2.0-16v-flexone-exl-4p-cvt-wmimagem15252259135.jpg?s=fill&w=552&h=414&q=60"
-            alt="carro"
-          />
-
-          <p className="font-bold mt-1 mb-2 px-2">CIVIC</p>
-
-          <div className="flex flex-col px-2">
-            <span className="text-zinc-700 mb-7 ">Ano 2016 | 23.000 KM</span>
-            <strong className="text-black text-xl font-medium">
-              R$ 190.000
-            </strong>
-
-            <div className="w-full h-px bg-slate-300 my-2"></div>
-
-            <div className="px-2 pb-2">
-              <span className="text-black">Toritama-PE</span>
-            </div>
-          </div>
-        </section>
+          </section>
+        ))}
       </main>
     </Container>
   );
