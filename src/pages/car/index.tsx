0e355fd30ec6801048a1d84ffcb26405056f41bc
Tacianojr07 <t.juniior13@gmail.com>
2023-11-13 16:54:22 -0300

    push

--- src/pages/car/index.tsx
+++ src/pages/car/index.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { FaWhatsapp } from "react-icons/fa";
 import { Container } from "../../components/container";
+import { getDoc, doc } from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
 
 interface CarProps {
   name: string;
@@ -27,13 +29,42 @@ interface CarImagesProps {
 
 export function Car() {
   const { id } = useParams();
-  const { car, setCar } = useState<CarProps>();
+  const { car, setCar } = useState<CarProps[]>();
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    async function loadCar() {
+      if (!id) {
+        return;
+      }
+
+      const docRef = doc(db, "cars", id);
+      getDoc(docRef).then((snapshot) => {
+        setCar({
+          id: snapshot.id,
+          name: snapshot.data()?.name,
+          year: snapshot.data()?.year,
+          km: snapshot.data()?.km,
+          price: snapshot.data()?.price,
+          city: snapshot.data()?.city,
+          images: snapshot.data()?.images,
+          uid: snapshot.data()?.uid,
+          owner: snapshot.data()?.owner,
+          description: snapshot.data()?.description,
+          date: snapshot.data()?.date,
+          model: snapshot.data()?.model,
+          whatsapp: snapshot.data()?.whatsapp,
+        });
+      });
+
+      console.log(car);
+    }
+
+    loadCar();
+  }, []);
 
   return (
-    <div>
+    <Container>
       <h1>page details</h1>
-    </div>
+    </Container>
   );
 }
